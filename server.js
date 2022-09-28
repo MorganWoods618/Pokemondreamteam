254b1d94f320a70ca3526df5796074158abfb66f
Morgan Woods <morgan4242564@gmail.com>
2022-09-27 22:53:56 -0600

    got code to work enough to pull up signup page

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var cookieParser = require('cookie-parser');
-var session = require('express-session');
+//////had to comment out both
+// var cookieParser = require('cookie-parser');
+// var session = require('express-session');
 // var morgan = require('morgan');
 // var User = require('./models/user');
 var hbs = require('express-handlebars'); 
@@ -49,18 +50,21 @@ app.use(require('./routes/api/characterRoutes'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // initialize cookie-parser to allow us access the cookies stored in the browser. 
-app.use(cookieParser());
+//////had to comment out
+
+// app.use(cookieParser());
 
 // initialize express-session to allow us track the logged-in user across sessions.
-app.use(session({
-    key: 'user_sid',
-    secret: 'somerandonstuffs',
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-        expires: 600000
-    }
-}));
+//////had to comment out
+// app.use(session({
+//     key: 'user_sid',
+//     secret: 'somerandonstuffs',
+//     resave: false,
+//     saveUninitialized: false,
+//     cookie: {
+//         expires: 600000
+//     }
+// }));
 
 // handle bars config
 app.engine('hbs', hbs({extname: 'hbs',defaultLayout: 'layout', layoutsDir: __dirname + '/views/layouts/'})); 
@@ -68,12 +72,13 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs'); 
 // This middleware will check if user's cookie is still saved in browser and user is not set, then automatically log the user out.
 // This usually happens when you stop your express server after login, your cookie still remains saved in the browser.
-app.use((req, res, next) => {
-    if (req.cookies.user_sid && !req.session.user) {
-        res.clearCookie('user_sid');        
-    }
-    next();
-});
+////////////////had to comment out
+// app.use((req, res, next) => {
+//     if (req.cookies.user_sid && !req.session.user) {
+//         res.clearCookie('user_sid');        
+//     }
+//     next();
+// });
 
 var hbsContent = {userName: '', loggedin: false, title: "You are not logged in today", body: "Hello World"}; 
 
